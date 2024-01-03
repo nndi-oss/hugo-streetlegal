ec16b33af9dd79bb3f1bd42bca90be9669ea49b3
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2024-01-03 04:28:19 +0200

    Initial commit

---
+++ archetypes/_default.md
@@ -0,0 +1,11 @@
++++
+title = ""
+lastUpdatedAt = "01/Jan/2024"
+publicationDate = "01/Jan/2024"
+effectiveDate = "01/Jan/2024"
+draft = ""
+product = ""
+breadcrumb = true
+toc = true
+webAddress = "url"
++++
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./exampleSite/**/*.{html,js}",
+    "./content/**/*.{html,js}",
+    "./layouts/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
